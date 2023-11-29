025b6167c5595df53a1b1877d282c10f44792c3c
mohamedeliwa <mohamed.av11@gmail.com>
2023-11-29 22:57:24 +0200

    created users module to replace bikers and senders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { SendersModule } from './senders/senders.module';
 import { BikersModule } from './bikers/bikers.module';
 import { ParcelsModule } from './parcels/parcels.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [SendersModule, BikersModule, ParcelsModule],
+  imports: [SendersModule, BikersModule, ParcelsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
